ad9e5ef02649e04f9ea540e2e226c5d0fd956186
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-08-22 12:24:39 +0530

    header

--- 05-list-reservation/src/main.cpp
+++ 05-list-reservation/src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
-#include "print.hpp"
 #include "flight.hpp"
+#include "file.hpp"
+
+void place(std::vector<std::string> &v, std::string s)
+{
+    v.push_back(s);
+}
 
 int main(int argc, char *argv[])
 {
@@ -18,5 +24,55 @@ int main(int argc, char *argv[])
     flights[0].remove({"Raja",  "Mahima"});
     lib::print(flights[0].getPassengers().size());
     flights[0].print();
+    lib::print(lib::readFile("input.txt"));
+    // std::ifstream t("input.txt");
+    // std::string str((std::istreambuf_iterator<char>(t)),
+    //                 std::istreambuf_iterator<char>());
+
+    // std::string delimiter = "\n";
+
+    // size_t pos = 0;
+    // std::string token;
+    // int flag = 0;
+    // std::vector<std::string> vec;
+    // while ((pos = str.find(delimiter)) != std::string::npos)
+    // {
+    //     token = str.substr(0, pos);
+
+    //     vec.push_back(std::string(token));
+    //     // lib::print(vec[0]);
+    //     // std::cout << token << std::endl;
+    //     str.erase(0, pos + delimiter.length());
+    //     if (!(pos = str.find(delimiter)) != std::string::npos)
+    //     {
+    //         for (size_t i = 0; i < vec.size(); i++)
+    //         {
+    //             lib::print(vec[i]);
+    //         }
+    //     }
+    // }
+
+    // std::cout << str << std::endl;
+    // int count = 0;
+    // std::string temp = "";
+    // std::vector<std::string> vec;
+    // vec.reserve(10);
+    // for (size_t i = 0; i < str.size(); i++)
+    // {
+    //     if (str[i] == '\n')
+    //     {
+    //         vec[count]=temp;
+    //         count++;
+    //         temp="";
+    //         continue;
+    //     }
+    //     temp += str[i];
+    // }
+
+    // for (size_t i = 0; i < vec.size(); i++)
+    // {
+    //     std::cout << vec[i];
+    // }
+
     return 0;
 }
---
+++ headers/file.hpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <iostream>
+
+namespace lib
+{
+
+const std::string readFile(std::string filename)
+{
+    std::ifstream file;
+    std::string line;
+    // std::stringstream ss;
+    char x;
+    file.open(filename);
+    std::string str = "";
+    for (std::string line; getline(file, line);)
+    {
+        std::cout << line;
+    }
+    return str;
+}
+
+std::vector<std::string> split_string(std::string filename,
+                                      const std::string &delimiter)
+{
+    const std::string &str = readFile(filename);
+
+    std::vector<std::string> strings;
+
+    std::string::size_type pos = 0;
+    std::string::size_type prev = 0;
+    while ((pos = str.find(delimiter, prev)) != std::string::npos)
+    {
+        strings.push_back(str.substr(prev, pos - prev));
+        prev = pos + 1;
+    }
+
+    // To get the last substring (or only, if delimiter is not found)
+    strings.push_back(str.substr(prev));
+
+    return strings;
+}
+
+} // namespace lib
